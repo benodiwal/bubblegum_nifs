7b2faafddb3d5bd108312fb02fb807b5d069e188
benodiwal <s474996633@gmail.com>
2025-03-13 17:59:10 +0530

    feat: update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BubblegumNifs.MixProject do
   def project do
     [
       app: :bubblegum_nifs,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
